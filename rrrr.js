41edc036f4e51019b7ad00733a6df91272c90c72
Zaid Baida <87798207+Zaidbaidadekalb@users.noreply.github.com>
2022-08-14 18:37:25 -0400

    Create rrrr.js

---
+++ rrrr.js
@@ -0,0 +1,179 @@
+
+var link = "https://www.codegrepper.com/code-examples/javascript/how+to+position+a+button+in+javascript"
+var vengelink = "https://venge.io/"
+
+
+if (window.location.href === link){
+alert("correct link");
+var key = "Zaid"
+var link2 = "https://www.codegrepper.com/code-examples/javascript/how+to+position+a+button+in+javascript"
+if (confirm("do you want to get key if not click cancel ")) {
+  txt = alert("OK");
+document.write('<!DOCTYPE html>');
+document.write('<html>');
+document.write('<head>');
+document.write('<meta name="viewport" content="width=device-width, initial-scale=1">');
+document.write('<style>');
+document.write('.tooltip {');
+document.write('  position: relative;');
+document.write('  display: inline-block;');
+document.write('}');
+document.write('');
+document.write('.tooltip .tooltiptext {');
+document.write('  visibility: hidden;');
+document.write('  width: 140px;');
+document.write('  background-color: #555;');
+document.write('  color: #fff;');
+document.write('  text-align: center;');
+document.write('  border-radius: 6px;');
+document.write('  padding: 5px;');
+document.write('  position: absolute;');
+document.write('  z-index: 1;');
+document.write('  bottom: 150%;');
+document.write('  left: 50%;');
+document.write('  margin-left: -75px;');
+document.write('  opacity: 0;');
+document.write('  transition: opacity 0.3s;');
+document.write('}');
+document.write('');
+document.write('.tooltip .tooltiptext::after {');
+document.write('  content: "";');
+document.write('  position: absolute;');
+document.write('  top: 100%;');
+document.write('  left: 50%;');
+document.write('  margin-left: -5px;');
+document.write('  border-width: 5px;');
+document.write('  border-style: solid;');
+document.write('  border-color: #555 transparent transparent transparent;');
+document.write('}');
+document.write('');
+document.write('.tooltip:hover .tooltiptext {');
+document.write('  visibility: visible;');
+document.write('  opacity: 1;');
+document.write('}');
+document.write('</style>');
+document.write('</head>');
+document.write('<body>');
+document.write('');
+document.write('<p>Just go to the pastebin then get key then Refreash page then put key .</p>');
+document.write('');
+document.write('<input type="text" value="https://pastebin.com/raw/RqsSfL9j" id="myInput">');
+document.write('');
+document.write('<div class="tooltip">');
+document.write('<button onclick="myFunction()" onmouseout="outFunc()">');
+document.write('  <span class="tooltiptext" id="myTooltip">Copy to clipboard</span>');
+document.write('  Copy text');
+document.write('  </button>');
+document.write('</div>');
+document.write('');
+document.write('<script>');
+document.write('function myFunction() {');
+document.write('  var copyText = document.getElementById("myInput");');
+document.write('  copyText.select();');
+document.write('  copyText.setSelectionRange(0, 99999);');
+document.write('  navigator.clipboard.writeText(copyText.value);');
+document.write('  ');
+document.write('  var tooltip = document.getElementById("myTooltip");');
+document.write('  tooltip.innerHTML = "Copied: " + copyText.value;');
+document.write('}');
+document.write('');
+document.write('function outFunc() {');
+document.write('  var tooltip = document.getElementById("myTooltip");');
+document.write('  tooltip.innerHTML = "Copy to clipboard";');
+document.write('}');
+document.write('</script>');
+document.write('</body>');
+document.write('</html>');
+document.write('');
+
+} else {
+  txt = alert
+
+
+}
+
+if (confirm("Make sure you have the correct key if not click cancel here is the key:  " + key)) {
+  txt = alert
+} else {
+  txt = alert("You clicked cancel");
+div.remove()
+
+}
+
+var version = "the version is 0.6"
+
+let div = document.createElement("div");
+let btn = document.createElement("button");
+let btn2 = document.createElement("button");
+let vengeb = document.createElement("button");
+vengeb.style.opacity = "0";
+
+
+btn.innerHTML = "version";
+btn2.innerHTML = "Start";
+vengeb.innerHTML = "Infinite Jump, Infinite ammo, Zero Recoil and custom fov for Venge.io!";
+
+
+div.id = "div_id" ;
+    div.className = "div_class";
+    div.style = "background-color:#2c2c2c;";
+    div.style.width = "300px";
+    div.style.height = "300px";
+    document.body.appendChild(div);  
+    div.style.position = "absolute";
+    div.style.top =  "440px"; //or whatever 
+    div.style.left = "1500px"; // or whatever
+    div.style.borderRadius = "5px";
+    btn.style.borderRadius = "5px";
+    btn.style.position = "absolute";
+    btn2.style.position = "absolute";
+    btn.style.marginLeft = "5px";
+    btn2.style.marginLeft = "240px";
+    btn2.style.marginTop = "277px";
+    btn2.style.borderRadius = "5px";
+    btn.style.marginTop = "5px";
+    btn2.style.width  = '58.95px';
+    btn2.style.hight = '21px';
+    vengeb.style.marginTop = '267px'
+    vengeb.style.marginLeft = "240px";
+
+
+
+    
+btn.onclick = function () {
+  alert(version);
+};
+// key \\
+if (key === "Zaid") {
+alert("You have the correct key");
+}
+
+vengeb.onclick = function () {
+
+if (window.location.href === vengelink){
+alert("Thank you Krunkador For cheats");
+} else {
+alert("Wrong game or webise make sure ur at venge lobby or else it will not work");
+}
+}
+btn2.onclick = function () {
+
+vengeb.style.opacity = "0";
+
+}
+     
+if (key != "Zaid") {
+div.remove()
+
+
+alert("wrong key or it might be that you just dident type anyhting in the keybox");
+
+}
+
+//End of Key \\
+div_id.appendChild(btn);
+div_id.appendChild(btn2);
+div_id.appendChild(vengeb);
+
+    
+}
