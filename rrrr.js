79de5ba6d0546cabd0e765dc8a40567ceed6a1e7
Zaid Baida <87798207+Zaidbaidadekalb@users.noreply.github.com>
2022-08-18 22:33:39 -0400

    Update rrrr.js

--- rrrr.js
+++ rrrr.js
@@ -122,7 +122,8 @@ div.id = "div_id" ;
     div.style = "background-color:#2c2c2c;";
     div.style.width = "300px";
     div.style.height = "300px";
-    document.querySelector("#wrapper").appendChild(div);  
+    document.body.appendChild(div);
+    div.style.zIndex = "10000000";  
     div.style.position = "fixed";
     div.style.top =  "440px"; //or whatever 
     div.style.left = "1500px"; // or whatever
