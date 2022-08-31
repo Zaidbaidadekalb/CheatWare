d093117b3ed631a8872ec5806c7701bc4c8c1dc3
Zaid Baida <87798207+Zaidbaidadekalb@users.noreply.github.com>
2022-08-31 17:54:02 -0400

    Delete Cheat ui (bookmark).js

--- Cheat ui (bookmark).js
+++
@@ -1,1 +0,0 @@
-javascript: var key = "Zaid"; (function(){window.s0=document.createElement('script');window.s0.setAttribute('type','text/javascript');window.s0.setAttribute('src','https://bookmarkify.it/bookmarklets/51998/raw');document.getElementsByTagName('body')[0].appendChild(window.s0);})();
